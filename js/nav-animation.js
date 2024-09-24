1beb5ad38e7871e774f7013ce8e2810c797ff899
EstruttiJP <716cobre@gmail.com>
2024-09-23 22:25:40 -0300

    Update responsive version

--- js/nav-animation.js
+++ js/nav-animation.js
@@ -1,16 +1,21 @@
 const navmenu = document.querySelector(".nav-menu");
 navmenu.onclick = function () {
     document.querySelector('nav').classList.toggle('ativo');
+    document.querySelector('.usuario').classList.toggle('ativo');
 }
-const sections = document.querySelectorAll('.section');
+const sections = document.querySelectorAll('.section'); //home, camisas, bandeiras, logos
 const navItems = document.querySelectorAll('.sidebar ul a li');
+const vetorSections = ["home", "camisas", "bandeiras", "logos"];
+
 window.addEventListener('scroll', () => {
     let currentSection = '';
+    let nextSection = null;
 
-    sections.forEach(section => {
+    sections.forEach((section, index) => {
         const sectionTop = section.offsetTop;
         if (pageYOffset >= sectionTop - 60) {
             currentSection = section.getAttribute('id');
+            nextSection = sections[index + 1];
         }
     });
 
@@ -20,4 +25,11 @@ window.addEventListener('scroll', () => {
             li.classList.add('selecionado');
         }
     });
+
+    if (nextSection) {
+        if (nextSection=="home") {
+            nextSection.style.animation="5s Surgir ease-in-out";
+        }
+        nextSection.classList.add('animation-surgir');
+    }
 });
